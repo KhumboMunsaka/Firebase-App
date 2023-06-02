45a5edddb30fcd617ce6a733692a1a6c5d9a8676
KhumboMunsaka <103140158+KhumboMunsaka@users.noreply.github.com>
2023-06-02 15:53:23 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -3,8 +3,9 @@ import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.15.0/firebas
 import {
   getDatabase,
   ref,
-  onValue,
   push,
+  onValue,
+  remove,
 } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js';
 
 const appSettings = {
@@ -17,26 +18,42 @@ const itemsInBasket = ref(database, 'items-in-basket');
 const basketInputEl = document.getElementById('input-field');
 const addToBasketEl = document.getElementById('add-button');
 const shoppingList = document.getElementById('shopping-list');
-let inputValue = basketInputEl.value;
 
 onValue(itemsInBasket, function (snapshot) {
-  let basketArray = Object.values(snapshot.val());
-  for (let i = 0; i < basketArray.length; i++) {
-    let currentItem = basketArray[i];
-    console.log(currentItem);
+  if (snapshot.exists()) {
+    let basketArray = Object.entries(snapshot.val());
+    shoppingList.innerHTML = '';
+    for (let i = 0; i < basketArray.length; i++) {
+      let currentItem = basketArray[i];
+      let currentItemId = currentItem[0];
+      let currentItemValue = currentItem[1];
+      appendItemToBasket(currentItem);
+    }
+  } else {
+    shoppingList.innerHTML = '<p class="noItem">No items in your basket yet :(</p> ';
   }
 });
 
 const clearInput = function () {
-  inputValue = '';
+  basketInputEl.value = '';
 };
-const appendItemToBasket = function () {
-  shoppingList.innerHTML += `<li>${inputValue}</li>`;
+const appendItemToBasket = function (item) {
+  let itemId = item[0];
+  let itemValue = item[1];
+
+  let newEl = document.createElement('li');
+  newEl.textContent = `${itemValue}`;
+  shoppingList.append(newEl);
+  // for deleting an item in the basket
+  newEl.addEventListener('dblclick', function () {
+    let exactLocationOfItem = ref(database, `items-in-basket/${itemId}`);
+    remove(exactLocationOfItem);
+  });
 };
 
 addToBasketEl.addEventListener('click', () => {
+  let inputValue = basketInputEl.value;
+  shoppingList.innerHTML = '';
   push(itemsInBasket, inputValue);
-
   clearInput();
-  appendItemToBasket();
 });
